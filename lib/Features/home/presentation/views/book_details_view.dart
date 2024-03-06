1c9855efc41f8c876a077b1caa3cd4dd04fcba71
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-03-06 16:25:57 +0200

    create book details view

---
+++ lib/Features/home/presentation/views/book_details_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:bookly/Features/home/presentation/views/book_details_view_body.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}
