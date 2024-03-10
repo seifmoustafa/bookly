e81927f6b8274b3fff7d4d6ae1c230d40199713f
Seif Moustafa <seif.moustafa516@gmail.com>
2024-03-10 16:56:31 +0200

    custom scroll view

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -15,82 +15,92 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Column(
-      children: [
-        const SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30.0),
-            child: CustomBookDetailsAppBar(),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: width * .2,
-            ),
-            child: const CustomBookImage(),
-          ),
-        ),
-        const SizedBox(
-          height: 43,
-        ),
-        const Text(
-          'The Jungle Book',
-          style: Styles.textStyle30,
-        ),
-        const SizedBox(
-          height: 6,
-        ),
-        Opacity(
-          opacity: .7,
-          child: Text(
-            'Rudyard Kipling',
-            style: Styles.textStyle18.copyWith(
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        ),
-        const SizedBox(
-          height: 18,
-        ),
-        const BookRating(
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
-        const SizedBox(
-          height: 37,
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30.0),
-          child: BooksAction(),
-        ),
-        const SizedBox(
-          height: 50,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 30.0),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle14.copyWith(
-                fontWeight: FontWeight.w600,
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Column(
+            children: [
+              const SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0),
+                  child: CustomBookDetailsAppBar(),
+                ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: width * .2,
+                  ),
+                  child: const CustomBookImage(),
+                ),
+              ),
+              const SizedBox(
+                height: 43,
+              ),
+              const Text(
+                'The Jungle Book',
+                style: Styles.textStyle30,
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              Opacity(
+                opacity: .7,
+                child: Text(
+                  'Rudyard Kipling',
+                  style: Styles.textStyle18.copyWith(
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              const BookRating(
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              const SizedBox(
+                height: 37,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                child: BooksAction(),
+              ),
+              const Expanded(
+                child: SizedBox(
+                  height: 50,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.textStyle14.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 20.0),
+                child: SimilarBooksListView(),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+            ],
           ),
-        ),
-        const SizedBox(
-          height: 16,
-        ),
-        const Padding(
-          padding: EdgeInsets.only(left: 20.0),
-          child: SimilarBooksListView(),
-        ),
-        // const SizedBox(
-        //   height: 40,
-        // ),
+        )
       ],
     );
+    // retur n ;
   }
 }
