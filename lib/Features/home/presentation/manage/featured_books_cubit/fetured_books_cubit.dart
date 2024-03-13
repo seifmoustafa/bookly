74bb5161e3da842f4c827cbe7a9e5167f2922a38
Seif Moustafa <seif.moustafa516@gmail.com>
2024-03-13 19:49:20 +0200

    create featured books method

--- lib/Features/home/presentation/manage/featured_books_cubit/fetured_books_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
-import 'package:equatable/equatable.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-part 'fetured_books_state.dart';
-
-class FeturedBooksCubit extends Cubit<FeturedBooksState> {
-  FeturedBooksCubit() : super(FeturedBooksInitial());
-}
